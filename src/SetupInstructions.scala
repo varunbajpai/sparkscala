739f2c7dc3de16a15a86acfc8a1b0f0223465306
Varun Bajpai <Varun.Bajpai@gartner.com>
2021-04-05 14:23:12 +0530

    adding more streaming stuff

---
+++ src/SetupInstructions.scala
@@ -0,0 +1,9 @@
+
+
+object SetupInstructions {
+  //Always remember to import jars in the class path
+  //Always see that the eclipse version is compatible with the hadoop version in use
+  
+  //in order to import jars do like add to build path and then add the jars
+  //in order to see if hadoop version is good, go to settings and check the hadoop version
+}
